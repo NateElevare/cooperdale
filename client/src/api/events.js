7412ce0f58685be64322bdbde0fdd0ca6f762980
NateElevare <109088991+NateElevare@users.noreply.github.com>
2026-04-19 10:37:58 -0400

    fixing move button

--- client/src/api/events.js
+++ client/src/api/events.js
@@ -3,5 +3,6 @@ import { http } from "./http";
 export const EventsApi = {
   list: () => http("/api/events"),
   create: (data) => http("/api/events", { method: "POST", body: JSON.stringify(data) }),
+  update: (id, data) => http(`/api/events/${id}`, { method: "PUT", body: JSON.stringify(data) }),
   remove: (id) => http(`/api/events/${id}`, { method: "DELETE" }),
 };
